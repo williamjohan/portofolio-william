9f8ef6b846658928da9492242e6d81e825ff5413
williamjohanp <williamjohanp@gmail.com>
2026-03-25 11:38:33 +0700

    fix discord well-known

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/discord",
+        destination: "/.well-known/discord.txt",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
